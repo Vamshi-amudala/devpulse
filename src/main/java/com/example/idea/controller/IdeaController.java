240de3fd3578151435b48296fae3b48fb99e6990
vamshi Amudala <vamshipatel126@gmail.com>
2026-03-04 00:14:54 +0530

    Idea service updated and exception added

--- src/main/java/com/example/idea/controller/IdeaController.java
+++ src/main/java/com/example/idea/controller/IdeaController.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,4 +44,29 @@ public class IdeaController {
         Pageable pageable = PageRequest.of(page, size);
         return new ResponseEntity<>(ideaService.getAllIdeas(pageable), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<IdeaResponse> getIdeaByid(@PathVariable Long id) {
+        return new ResponseEntity<>(ideaService.getIdeaById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteIdeaByID(@PathVariable Long id) {
+        ideaService.deleteIdeaById(id);
+        return ResponseEntity.ok("Idea deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<IdeaResponse> updateById(@PathVariable Long id,
+            @RequestBody IdeaRequest request) {
+
+        return new ResponseEntity<>(ideaService.updateIdeaById(id, request), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<IdeaResponse> patchById(@PathVariable Long id,
+            @RequestBody IdeaRequest request) {
+        return ResponseEntity.ok(ideaService.patchIdeaById(id, request));
+    }
+
 }
